33e6287865c7e51210cef90bcf6c4a6a2d71252d
Rashed <202305787@student.polytechnic>
2025-12-30 04:49:33 +0300

    design elements

--- AppPlateFul/TableCells/AvailableDonationCell.swift
+++ AppPlateFul/TableCells/AvailableDonationCell.swift
@@ -14,11 +14,28 @@ class AvailableDonationCell: UITableViewCell {
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var button: UIButton!
     
+    @IBOutlet weak var cardView: UIView!
     
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+          super.awakeFromNib()
+
+          cardView.layer.cornerRadius = 12
+          cardView.layer.masksToBounds = false
+
+          cardView.layer.shadowColor = UIColor.black.cgColor
+          cardView.layer.shadowOpacity = 0.15
+          cardView.layer.shadowOffset = CGSize(width: 0, height: 4)
+          cardView.layer.shadowRadius = 6
+      }
+
+      override func layoutSubviews() {
+          super.layoutSubviews()
+
+          cardView.layer.shadowPath = UIBezierPath(
+              roundedRect: cardView.bounds,
+              cornerRadius: 12
+          ).cgPath
+      }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
--- AppPlateFul/TableCells/MyPickupsTableViewCell.swift
+++ AppPlateFul/TableCells/MyPickupsTableViewCell.swift
@@ -15,13 +15,30 @@ class MyPickupsTableViewCell: UITableViewCell {
     @IBOutlet weak var ViewDetailsBtn: UIButton!
     @IBOutlet weak var statusBtn: UIButton!
     
- 
+    @IBOutlet weak var cardView: UIView!
+    
     
     
     override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+          super.awakeFromNib()
+
+          cardView.layer.cornerRadius = 12
+          cardView.layer.masksToBounds = false
+
+          cardView.layer.shadowColor = UIColor.black.cgColor
+          cardView.layer.shadowOpacity = 0.15
+          cardView.layer.shadowOffset = CGSize(width: 0, height: 4)
+          cardView.layer.shadowRadius = 6
+      }
+
+      override func layoutSubviews() {
+          super.layoutSubviews()
+
+          cardView.layer.shadowPath = UIBezierPath(
+              roundedRect: cardView.bounds,
+              cornerRadius: 12
+          ).cgPath
+      }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
